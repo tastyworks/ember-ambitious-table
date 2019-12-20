107c52be7c69cf88209a083a35d2582da1218f3d
Kevin Tung <kekekevin@gmail.com>
2019-12-20 14:50:34 -0600

    utilize ember getters since the hash can be an ember object

--- addon/helpers/amb-table-style.js
+++ addon/helpers/amb-table-style.js
@@ -3,11 +3,11 @@ import Ember from 'ember'
 export function style (_params, hash) {
   let values = []
   for (let key in hash) {
-    if (!hash.hasOwnProperty(key)) {
+    let value = Ember.get(hash, key)
+    if (Ember.isEmpty(value) {
       return
     }
 
-    let value = hash[key]
     if (Number(value)) {
       value = `${value}px`
     }
